e2a81233ee23812ed97c32b5156b193978a5b068
v200dd <101986982+v200dd@users.noreply.github.com>
2026-01-09 02:05:52 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,11 +20,11 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>项目:</strong><a href="" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>作者:</strong><a href="" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
-      <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
+      <strong>初始项目:</strong><a href="" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
 
@@ -40,10 +40,10 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
+      homeUrl: "",
+      blogUrl: "",
+      githubUrl: "",
+      emailUrl: ""
     };
   }
 };
